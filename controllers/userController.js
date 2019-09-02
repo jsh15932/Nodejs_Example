de40d5d125dcbc8ebf8f2b77da8ff84790367d1a
장승훈 <jsh1400512@naver.com>
2019-09-02 14:47:11 +0900

    Update Upload

--- controllers/userController.js
+++ controllers/userController.js
@@ -18,13 +18,17 @@ export const postJoin = (req, res) => {
     }
 };
 
-export const getLogin = (req, res) => res.render("login", { pageTitle: "Log In" });
+export const getLogin = (req, res) => 
+res.render("login", { pageTitle: "Log In" });
 export const postLogin = (req, res) => {
     res.redirect(routes.home);
 }
 
-export const logout = (req, res) =>
-render("logout", { pageTitle: "Log Out" });
+export const logout = (req, res) => {
+    // To Do: Process Log Out
+    res.redirect(routes.home);
+}
+
 export const userDetail = (req, res) =>
 render("userDetail", { pageTitle: "User Detail" });
 export const editProfile = (req, res) =>
--- routers/globalRouter.js
+++ routers/globalRouter.js
@@ -17,7 +17,6 @@ globalRouter.post(routes.join, postJoin);
 globalRouter.get(routes.login, getLogin);
 globalRouter.post(routes.login, postLogin);
 
-
 globalRouter.get(routes.home, home);
 globalRouter.get(routes.search, search);
 globalRouter.get(routes.logout, logout);
