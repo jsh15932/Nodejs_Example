6ce11825463314944a4f832a2a5f1f43dc67f13c
장승훈 <jsh1400512@naver.com>
2019-09-02 16:32:54 +0900

    Update DB

--- db.js
+++ db.js
@@ -1,38 +1,19 @@
-export const videos = [
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
+mongoose.connect(
+    process.env.MONGO_URL,
     {
-        id:324393,
-        title:'Video awesome',
-        description:'This is something I love',
-        views:24,
-        videoFile:"http://media.w3.org/2010/05/bunny/movie.ogv",
-        creators: {
-            id:121212,
-            name:"SeungHoon",
-            email:"jsh1400512@naver.com"
-        }
-    },
-    {
-        id:123121,
-        title:'Video super',
-        description:'This is something I love',
-        views:24,
-        videoFile:"http://media.w3.org/2010/05/bunny/movie.ogv",
-        creators: {
-            id:121212,
-            name:"SeungHoon",
-            email:"jsh1400512@naver.com"
-        }
-    },
-    {
-        id:343532,
-        title:'Video nice',
-        description:'This is something I love',
-        views:24,
-        videoFile:"http://media.w3.org/2010/05/bunny/movie.ogv",
-        creators: {
-            id:121212,
-            name:"SeungHoon",
-            email:"jsh1400512@naver.com"
-        }
+        useNewUrlParser: true,
+        useFindAndModify: false
     }
-];
+);
+
+const db = mongoose.connection;
+
+const handleOpen = () => console.log("Connected to DB");
+const handleError = (error) => console.log(`Error on DB Connection:${errror}`);
+
+db.once("open", handleOpen);
+db.on("error", handleError);
